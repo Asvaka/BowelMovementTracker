7148291c417b1b17b8668c6be037afaec3bbc8ab
Asvaka <78432238+Asvaka@users.noreply.github.com>
2026-01-09 21:24:17 -0800

    Initial UI Basis
    Really just removing the "Greeting" which the activity template put in

--- app/src/main/java/com/example/bowelmovementtracker/MainActivity.kt
+++ app/src/main/java/com/example/bowelmovementtracker/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bowelmovementtracker.ui.theme.BowelMovementTrackerTheme
 
@@ -20,8 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BowelMovementTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TrackerMainPage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +33,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TrackerMainPage(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        BowelMovementCalendar(Modifier.weight(0.5F))
+
+        Box(Modifier.weight(0.5F)) {
+            Text(
+                text = "This is where the details of each individual day will go"
+            )
+        }
+    }
+}
+
+@Composable
+fun BowelMovementCalendar(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(
+            text = "This is where the calendar will go",
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPagePreview() {
     BowelMovementTrackerTheme {
-        Greeting("Android")
+        TrackerMainPage()
     }
 }
